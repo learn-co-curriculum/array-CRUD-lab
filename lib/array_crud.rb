6bcd63369d0622694c1ca31866218f794baa3cfa
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-23 02:24:59 -0500

    commit tree 73c381d5e244cfc008d596027efcc77437c4440b

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,38 @@
 def create_an_empty_array
-  
+    emptyArray = []
+    emptyArray
 end
 
 def create_an_array
-  
+    fourElementArray = ["Junaid", "Akbar", "Abbasi", "24"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+    array << element
+    array
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array.unshift(element)
+    array
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array.pop()
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array[-1]
 end
